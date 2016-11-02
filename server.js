e47f9edcd14209ca0b410bdc0d7d3500564bb0de
Philip Lin <philipo227@hotmail.com>
2016-11-01 21:29:54 -0700

    setting up index.html,app.js, and angularjs

--- server.js
+++ server.js
@@ -9,3 +9,24 @@ app.use(express.static('public'))
 
 //INDEX OF ALL POKEMON
 // http://pokeapi.co/api/v2/pokemon/?limit=811
+app.get('/api/pokedex', function(req,res){
+  var apiUrl = 'http://pokeapi.co/api/v2/pokemon/?limit=811'
+
+  request.get(apiUrl, function(err,response,body){
+    res.json(JSON.parse(body))
+  })
+})
+
+//show all pokemon
+// http://pokeapi.co/api/v2/pokemon/1/
+app.get('/api/pokedex/:id', function(req,res){
+  var apiUrl = 'http://pokeapi.co/api/v2/pokemon/1/'
+  request.get(apiUrl, function(err,response,body){
+    res.json(JSON.parse(body))
+  })
+})
+
+
+app.listen(PORT, function(err){
+  console.log(err || "server running on " + PORT);
+})
